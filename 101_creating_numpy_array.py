e3739f91ab0a537ea7a2d01f9be685fc82887cb1
Anushri Namdeorao More <40488560+Anushri1903@users.noreply.github.com>
2023-01-29 22:29:56 -0500

    Add files via upload

---
+++ 101_creating_numpy_array.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 29 20:55:34 2023
+
+@author: anushrimore
+"""
+
---
+++ 102_numpy_operations.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 29 21:29:15 2023
+
+@author: anushrimore
+"""
+
+s
